0b681f74f606b3a42de7979ecb812064853a7f40
Kedar Vijay Kulkarni <kedarvijaykulkarni@gmail.com>
2021-09-26 18:52:28 +0530

    update the error message with help method example

--- src/config/error-message.js
+++ src/config/error-message.js
@@ -1,7 +1,7 @@
 module.exports = {
-  errorMessages () {
+  errorMessages() {
     return {
-      "access_token_missing": "Missing Access Token. required header `Authorization`, please get `bearer_id` using .accessTokenLogin(username, password) method. for more information about .accessTokenLogin function visit https://bit.ly/3mbCX5F"
+      "access_token_missing": "Missing Access Token. required header `Authorization`, please get `bearer_id` using .Auth().accessTokenLogin({ username: email@gmail.com , password: p@ssWord! }) method. for more information about .accessTokenLogin function visit https://bit.ly/3mbCX5F"
     }
   }
 };
