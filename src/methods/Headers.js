75e98d9ff4c5e04633a14e037b7af887b035fe19
Kedar Vijay Kulkarni <kedarvijaykulkarni@gmail.com>
2021-09-26 18:53:48 +0530

    seperate the header and fetch method from the code to reduce the dublicate code

---
+++ src/methods/Headers.js
@@ -0,0 +1,23 @@
+module.exports = class {
+  constructor(api_key, organization) {
+    this.api_key = api_key;
+    this.organization = organization;
+  }
+
+  getHeaders() {
+    let headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    };
+
+    if (this.api_key) {
+      headers["Authorization"] = `Bearer ${this.api_key}`;
+    }
+
+    if (this.organization) {
+      headers["OpenAI-Organization"] = this.organization;
+    }
+
+    return headers;
+  }
+};
