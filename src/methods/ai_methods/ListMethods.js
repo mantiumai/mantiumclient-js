9439c125a99846663b3d4e1227bd3334ae90e7e5
Kedar Vijay Kulkarni <kedarvijaykulkarni@gmail.com>
2021-09-26 18:58:01 +0530

    Update the methods for AI Methods (+)
    1. add ListMethods

---
+++ src/methods/ai_methods/ListMethods.js
@@ -0,0 +1,15 @@
+const config = require('../../config/config');
+const msg = require('../../config/error-message');
+const fetch = require('../fetch');
+const utils = require('../utility');
+
+module.exports = function (headers, queryParam, aiProvider) {
+  if (!headers.api_key) throw new Error(msg.errorMessages().access_token_missing);
+  let options = {
+    method: "GET",
+    url: config.aiMethodsURL().concat('/', aiProvider, utils.objToQueryStr(queryParam)),
+    headers: headers.getHeaders(),
+  };
+
+  return fetch(options);
+};
