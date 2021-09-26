3bb260ea8b092197cfb75e880d70d8f9faba5f3f
Kedar Vijay Kulkarni <kedarvijaykulkarni@gmail.com>
2021-09-26 18:56:07 +0530

    Update the methods for Auth (+)
    1. Login method
    2. Logout method
    3. ResetPassword method

---
+++ src/methods/auth/ResetPassword.js
@@ -0,0 +1,22 @@
+const config = require('../../config/config');
+const msg = require('../../config/error-message');
+const fetch = require('../fetch');
+
+module.exports = function (
+  headers,
+  {
+    email
+  } = {}
+) {
+  if (!headers.api_key) throw new Error(msg.errorMessages().access_token_missing);
+  let options = {
+    method: "POST",
+    url: config.resetPasswordURL(),
+    headers: headers.getHeaders(),
+    body: JSON.stringify({
+      email
+    }),
+  };
+
+  return fetch(options);
+};
